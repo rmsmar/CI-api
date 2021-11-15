111cf32d69b5de8d4e5e104ccc084812740b9adc
rmsmar <rms.marina89@gmail.com>
2021-11-15 22:34:58 +0300

    return the current test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -16,8 +16,6 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200)
-          .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-      ;
+          .statusCode(200);
     }
 }
